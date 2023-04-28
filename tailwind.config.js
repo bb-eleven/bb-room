650e32add493a5dba50b8cc2370c9fc69c965d44
Xavier Ang <xaveang@gmail.com>
2023-04-28 20:47:34 +0800

    Add theme from game-of-life
    Add theme from game-of-life

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'pine-glade': '#C7D19E',
+				'ship-cove': '#6E91B9',
+				'mirage': {
+					DEFAULT: '#191F2D',
+					darker: '#151B29',
+					lighter: '#1C2231'
+				},
+				'cadet-blue': '#A0A8C3',
+				'comet': {
+					DEFAULT: '#636985',
+					darker: '#434965'
+				},
+				'orange-red-crayola': '#FF5A5F',
+				'cultured': '#F5F5F5',
+				'envy': '#93B19C',
+				'quicksand': '#B99090'
+			},
+			fontFamily: {
+				logo: 'MeshedDisplay-SemiBold',
+				text: 'BLMelody-Regular'
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 };
