ec32f3154270f4d435ef6264d573c82033c1903c
Xavier Ang <xaveang@gmail.com>
2023-05-02 14:09:30 +0800

    Make SearchBar placeholder configurable

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	export let search: string;
+  export let placeholder = 'Search';
 </script>
 
 <div class="{$$props.class}">
   <label>
     <input
       type="text"
-      placeholder="Search"
+      {placeholder}
       spellcheck="false"
       bind:value={search}
       class="w-full bg-mirage placeholder:text-comet-darker text-comet select-none rounded-md border-2 border-comet p-1 pl-2 active:outline-none focus:outline-none"
