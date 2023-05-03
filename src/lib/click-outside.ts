0a0e26c235617c5c09faaa545d9bef9a613b5cd7
Xavier Ang <xaveang@gmail.com>
2023-05-03 11:36:37 +0800

    Fix event not propagating on own element click

--- src/lib/click-outside.ts
+++ src/lib/click-outside.ts
@@ -3,10 +3,9 @@
 export const clickOutside = (element: HTMLElement, callback: Function, include?: string) => {
 
   const handleClick = (event: Event) => {
-    event.stopPropagation();
-
     const target = event.target as HTMLElement;
     if (element && !element.contains(target) && !event.defaultPrevented) {
+      event.stopPropagation();
       callback();
     }
   }
