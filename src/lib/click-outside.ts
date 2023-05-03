94a2a0211dc7dfe8e95be8169da3fb4272c73dd5
Xavier Ang <xaveang@gmail.com>
2023-05-03 11:08:12 +0800

    Don't ignore parent elements in clickOutside event

--- src/lib/click-outside.ts
+++ src/lib/click-outside.ts
@@ -1,7 +1,10 @@
-/** Dispatch event on click outside of node */
-export const clickOutside = (element: HTMLElement, callback: Function) => {
+// Dispatch event on click outside of node
+// https://github.com/sveltejs/svelte/issues/3012
+export const clickOutside = (element: HTMLElement, callback: Function, include?: string) => {
 
   const handleClick = (event: Event) => {
+    event.stopPropagation();
+
     const target = event.target as HTMLElement;
     if (element && !element.contains(target) && !event.defaultPrevented) {
       callback();
