c6280b67eb322e9f687acbc26d0938f91919e6fa
Xavier Ang <xaveang@gmail.com>
2023-05-10 18:31:47 +0800

    Add links in homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<div class="h-[100vh] flex items-center justify-center"><h1 class="text-4xl">It is da BB room</h1></div>
+<div class="h-[100vh] flex flex-col items-center justify-center">
+	<h1 class="text-4xl">It is da BB room</h1>
+	<a href="/view-items">View items</a>
+	<a href="/create-transaction">Create transaction</a>
+</div>
