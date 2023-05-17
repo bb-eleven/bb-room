8d4fe90e9711333e712978fede48cb03540b3f22
Xavier Ang <xaveang@gmail.com>
2023-05-18 00:42:47 +0800

    Update ninou type guard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
 export const inou = <T>(value: T | null | undefined): value is null | undefined =>
 	value === null || value === undefined;
 
-export const ninou = <T>(value: T | null | undefined): value is null | undefined => !inou(value);
+export const ninou = <T>(value: T | null | undefined): value is T => !inou(value);
