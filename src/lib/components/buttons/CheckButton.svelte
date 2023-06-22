015fb6b0abb3c08894450b54de780befbd7f880a
Xavier Ang <xaveang@gmail.com>
2023-06-23 07:02:17 +0800

    Add Check and X buttons

---
+++ src/lib/components/buttons/CheckButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Icon from '../icon/Icon.svelte';
+	import { IconSvg } from '../icon/icon-svg';
+
+	export let clicked = false;
+	export let click = () => {};
+</script>
+
+<button
+	on:click={() => {
+		clicked = !clicked;
+		click();
+	}}
+	class="w-10 p-2 rounded-full {clicked
+		? 'bg-off-600 fill-green-200'
+		: 'bg-off-500 fill-off-100 hover:fill-green-100'} {$$props.class}"
+>
+	<Icon icon={IconSvg.Check} class="" />
+</button>
