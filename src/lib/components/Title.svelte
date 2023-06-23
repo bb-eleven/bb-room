9c0a86d7fbbe313efa9c0708e614131e7fc72d77
Xavier Ang <xaveang@gmail.com>
2023-06-23 18:24:34 +0800

    Add Title component

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let title: string;
+	console.log(title);
+</script>
+
+<!-- min-width: 4 letters -->
+<div class="rounded-full w-fit min-w-[4rem] px-5 py-2 bg-brown-600 text-off-200 {$$props.class}">
+	<span>{title}</span>
+</div>
