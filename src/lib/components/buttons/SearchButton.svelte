b1851e65282449b033ba991e586bf91c9c9e7502
Xavier Ang <xaveang@gmail.com>
2023-06-23 17:02:09 +0800

    Add SearchButton

---
+++ src/lib/components/buttons/SearchButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Icon from '../icon/Icon.svelte';
+	import RotatingIcon from '../icon/RotatingIcon.svelte';
+	import { IconSvg } from '../icon/icon-svg';
+	import { defaultRotatingIconData, rotateIcon } from '../icon/rotating-icon';
+
+	// Collapsed by default
+	export let icon: IconSvg.ChevronUp | IconSvg.ChevronDown = IconSvg.ChevronDown;
+	export let click = () => {};
+
+	export let rotatingIconData = defaultRotatingIconData();
+</script>
+
+<button
+	on:click={() => {
+		rotatingIconData = rotateIcon(rotatingIconData);
+		click();
+	}}
+	class="py-2 px-3 rounded-full bg-blue-600 fill-blue-200 flex items-center gap-1 {$$props.class}"
+>
+	<Icon icon={IconSvg.Search} class="w-7" />
+	<RotatingIcon {icon} {rotatingIconData} class="w-6" />
+</button>
