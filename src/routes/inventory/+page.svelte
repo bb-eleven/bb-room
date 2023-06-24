0e1a353b7799c7bb7ed1854b5d7931ab5cc5f36b
Xavier Ang <xaveang@gmail.com>
2023-06-24 21:43:47 +0800

    Update inventory page with search button and inputs

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -3,24 +3,65 @@
 	import Title from '$lib/components/Title.svelte';
 	import SearchButton from '$lib/components/buttons/SearchButton.svelte';
 	import { SelectionButtonType } from '$lib/components/buttons/selection-button-type';
+	import Input from '$lib/components/inputs/Input.svelte';
+	import MultiSelect from '$lib/components/inputs/selects/MultiSelect.svelte';
+	import SingleSelect from '$lib/components/inputs/selects/SingleSelect.svelte';
 	import type { Database } from '$lib/database.types';
+	import { cubicOut } from 'svelte/easing';
+	import { slide } from 'svelte/transition';
 
-	const itemViews: Database['public']['Views']['item_view']['Row'][] = new Array(300).fill({
-		id: 1,
-		category_ids: [1, 2, 3],
-		category_names: ['1', '2', '3'],
-		location_codes: ['L1', 'tbc'],
-		location_quantities: [30, 0],
-		name: 'hello',
-		variant_name: null,
-	});
+	type ItemViewRow = Database['public']['Views']['item_view']['Row'];
+	const itemViews: ItemViewRow[] = new Array(300)
+		.fill({
+			id: 1,
+			category_ids: [1, 2, 3],
+			category_names: ['1', '2', '3'],
+			location_codes: ['L1', 'tbc'],
+			location_quantities: [30, 0],
+			name: 'hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello',
+			variant_name: null,
+		})
+		.map((itemView, i) => ({ ...itemView, name: [itemView.name, i].join(' ') }));
+	let multiSelectedLocations: any[] = [];
+	let showSearchInputs = false;
+
+	// TODO: update Supabase types
+	const getLocationOptionDisplay = (selected: ItemViewRow) => selected?.name ?? '';
+	const getLocationInputDisplay = (selected?: ItemViewRow) => selected?.name ?? 'Select';
+	const getLocationInputDisplayM = (selected: ItemViewRow[]) =>
+		selected.length === 0 ? 'Select' : selected.map(({ name }) => name).join(', ');
 </script>
 
 <!-- TODO: remove negative margin, 100vh(?) -->
-<div class="h-[100vh] bg-off-100 p-3 mt-[-0.5rem]">
-	<div class="flex items-end gap-2">
+<div class="relative h-[100vh] bg-off-100 p-3 mt-[-0.5rem]">
+	<div class="relative flex items-end gap-2">
 		<Title title="Inventory" />
-		<SearchButton />
+		<SearchButton click={() => (showSearchInputs = !showSearchInputs)} />
+
+		<!-- Search inputs -->
+		{#if showSearchInputs}
+			<div
+				transition:slide={{ duration: 500, easing: cubicOut }}
+				class="absolute z-50 top-16 rounded-2xl w-full p-3 space-y-4 bg-off-100 drop-shadow-2xl shadow-lg"
+			>
+				<Input label="Name" />
+				<SingleSelect
+					label="Locations"
+					selected={null}
+					getInputText={getLocationInputDisplay}
+					getOptionText={getLocationOptionDisplay}
+					options={itemViews}
+				/>
+
+				<MultiSelect
+					label="Locations"
+					bind:selected={multiSelectedLocations}
+					getInputText={getLocationInputDisplayM}
+					getOptionText={getLocationOptionDisplay}
+					options={itemViews}
+				/>
+			</div>
+		{/if}
 	</div>
 
 	<!-- Items -->
