54be3fc56697d4e9a9e4b2e14003daa87ac31635
Xavier Ang <xaveang@gmail.com>
2023-07-06 15:56:39 +0800

    index: Redirect to '/inventory'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,6 @@
 <script lang="ts">
-	import Login from '$lib/components/Login.svelte';
-	import { UsernameStore } from '$lib/stores/username';
-	import { inou, ninou } from '$lib/utils';
-	import type { PageData } from './$types';
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
 
-	export let data: PageData;
-	let username: string | null;
-	UsernameStore.subscribe((v) => (username = v));
+	if (browser) goto('/inventory');
 </script>
-
-<div class="h-[100vh] flex flex-col items-center justify-between">
-	<div class="h-full flex flex-col items-center justify-center gap-4">
-		<h1 class="text-4xl mb-4">It is da BB room</h1>
-		{#if inou(data.session)}
-			<Login supabase={data.supabase} />
-		{:else}
-			<a href="/view-items" class="underline decoration-solid">View items</a>
-			<a href="/create-transaction" class="underline decoration-solid">Create transaction</a>
-		{/if}
-	</div>
-	{#if ninou(username)}
-		<div class="mb-4">Logged in as "{username}"</div>
-	{/if}
-</div>
