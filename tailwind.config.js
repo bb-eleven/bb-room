5754e8f6186cc24fd009790e6081583c6709d543
Xavier Ang <xaveang@gmail.com>
2023-07-06 17:29:02 +0800

    Remove old components and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,30 +7,6 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				'pine-glade': '#C7D19E',
-				'ship-cove': '#6E91B9',
-				mirage: {
-					DEFAULT: '#191F2D',
-					dark: '#171D2B',
-					darker: '#151B29',
-					'darker-shadow': '#0A101E',
-					light: '#1C2231',
-					lighter: '#212739',
-					lightest: '#2A2F45',
-				},
-				'cadet-blue': '#A0A8C3',
-				comet: {
-					DEFAULT: '#636985',
-					darker: '#434965',
-				},
-				'orange-red-crayola': '#FF5A5F',
-				cultured: '#F5F5F5',
-				envy: '#88967B',
-				red: '#A25C5D',
-				quicksand: {
-					DEFAULT: '#B99090',
-					dark: '#A98080',
-				},
 				green: {
 					DEFAULT: '#63BB85',
 					100: '#B8E6C9',
@@ -123,10 +99,7 @@ module.exports = {
 					'100%': { transform: 'translateY(0)' },
 				},
 			},
-			fontFamily: {
-				logo: 'MeshedDisplay-SemiBold',
-				text: 'BLMelody-Regular',
-			},
+			fontFamily: {},
 		},
 	},
 	plugins: [],
