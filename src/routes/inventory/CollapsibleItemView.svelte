0b89152c76a33d4af0a0b9a82e62e2de07c431b6
Xavier Ang <xaveang@gmail.com>
2023-07-13 00:11:59 +0800

    inventory: Show categories on collapsed items

--- src/routes/inventory/CollapsibleItemView.svelte
+++ src/routes/inventory/CollapsibleItemView.svelte
@@ -21,8 +21,18 @@
 		{/if}
 	</svelte:fragment>
 
+	<svelte:fragment slot="body">
+		<div class="p-4">
+			<span class="text-off-600">Categories: </span>
+			<span class="text-off-700">{itemView.category_names?.join(', ')}</span>
+		</div>
+	</svelte:fragment>
+
 	<svelte:fragment slot="collapsible-body">
-		<div transition:slide={{ duration: 500, easing: cubicOut }} class="p-4 flex flex-col gap-4">
+		<div
+			transition:slide={{ duration: 500, easing: cubicOut }}
+			class="p-4 pt-0 flex flex-col gap-4"
+		>
 			<div>
 				<span class="text-off-600">Total quantity: </span>
 				<span class="text-off-700">{itemView.location_quantities?.reduce((a, b) => a + b)}</span>
@@ -30,10 +40,6 @@
 			<div>
 				<span class="text-off-600">Locations: </span>
 				<span class="text-off-700">{itemView.location_codes?.join(', ')}</span>
-			</div>
-			<div>
-				<span class="text-off-600">Categories: </span>
-				<span class="text-off-700">{itemView.category_names?.join(', ')}</span>
 			</div>
 		</div>
 	</svelte:fragment>
